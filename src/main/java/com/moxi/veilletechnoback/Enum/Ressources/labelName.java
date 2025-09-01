2efa004431a9f6cc28ff1d057f9b12ee67813c93
maxime lapouge <dpower632@gmail.com>
2025-09-01 10:38:03 +0200

    try to work with label enum

--- src/main/java/com/moxi/veilletechnoback/Enum/Ressources/labelName.java
+++ src/main/java/com/moxi/veilletechnoback/Enum/Ressources/labelName.java
@@ -1,5 +1,8 @@
 package com.moxi.veilletechnoback.Enum.Ressources;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+@JsonFormat(shape = JsonFormat.Shape.STRING)
 public enum labelName {
 	Docs,Example,Tutorial
 }
