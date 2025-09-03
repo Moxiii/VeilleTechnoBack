bbcba351fdd773ab0b7b745f70f78628478809f0
maxime lapouge <dpower632@gmail.com>
2025-09-03 20:33:34 +0200

    wip

--- src/main/java/com/moxi/veilletechnoback/Controller/TechnologyController.java
+++ src/main/java/com/moxi/veilletechnoback/Controller/TechnologyController.java
@@ -56,7 +56,7 @@ private TechnologyRes techToRes(Technology technology) {
 			basicProjects.add(basic);
 		}
 	}
-	res.setCreateAt(technology.getCreatedAt());
+	res.setCreatedAt(technology.getCreatedAt());
 	res.setProjects(basicProjects);
 	res.setTrainingTime(technology.getTrainingTime());
 	return res;
