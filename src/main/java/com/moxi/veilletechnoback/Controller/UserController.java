e63614cc93269c1a20798c1d902520a490061b44
maxime lapouge <dpower632@gmail.com>
2025-11-11 10:59:22 +0100

    work on pdf report

--- src/main/java/com/moxi/veilletechnoback/Controller/UserController.java
+++ src/main/java/com/moxi/veilletechnoback/Controller/UserController.java
@@ -2,6 +2,7 @@ package com.moxi.veilletechnoback.Controller;
 
 import com.itextpdf.text.DocumentException;
 import com.moxi.veilletechnoback.Config.Security.SecurityUtils;
+import com.moxi.veilletechnoback.Pdf.PdfReportOptions;
 import com.moxi.veilletechnoback.Pdf.PdfService;
 import com.moxi.veilletechnoback.User.User;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,11 +10,13 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.method.P;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.moxi.veilletechnoback.DTO.User.PROFILE.UserProfileRes;
-
+import com.moxi.veilletechnoback.Pdf.PdfReportOptions;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
@@ -38,9 +41,12 @@ public ResponseEntity<UserProfileRes> getUserProfile() {
 
 }
 @GetMapping("/generate/pdf")
-public ResponseEntity<byte[]> generatePDF() throws DocumentException, IOException {
+public ResponseEntity<byte[]> generatePDF(@RequestBody(required = false) PdfReportOptions options) throws DocumentException, IOException {
 	User currentUser = securityUtils.getCurrentUser();
-	ByteArrayInputStream bis = pdfService.generateUserReport(currentUser);
+	if(options == null){
+		options = new PdfReportOptions();
+	}
+	ByteArrayInputStream bis = pdfService.generateUserReport(currentUser , options);
 	HttpHeaders headers = new HttpHeaders();
 	headers.add("Content-Disposition", "inline; filename=rapport.pdf");
 	return ResponseEntity.ok().headers(headers).contentType(MediaType.APPLICATION_PDF).body(bis.readAllBytes());
